6bd3b82cb8b60420591caae01de57ae7015f391a
Angie <angiejschmitt@gmail.com>
2022-11-25 07:11:04 -0500

    Maybe this works?

--- index.js
+++ index.js
@@ -1,61 +1,52 @@
-// // Discord Stuff
-// const fs = require('node:fs');
-// const path = require('node:path');
+// Discord Stuff
+const fs = require('node:fs');
+const path = require('node:path');
 
-// // Require the necessary discord.js classes
-// const { Client, Partials, GatewayIntentBits, Collection } = require('discord.js');
-// // const { token } = require('/etc/secrets/config.json');
-// const { token } = require('./config.json');
+// Require the necessary discord.js classes
+const { Client, Partials, GatewayIntentBits, Collection } = require('discord.js');
+// const { token } = require('/etc/secrets/config.json');
+const { token } = require('./config.json');
 
-// // Create a new client instance
-// const client = new Client({
-// 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
-// 	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
-// });
+// Create a new client instance
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
 
-// // Assign Commands
-// client.commands = new Collection();
-// const commandsPath = path.join(__dirname, 'commands');
-// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+// Assign Commands
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-// for (const file of commandFiles) {
-// 	const filePath = path.join(commandsPath, file);
-// 	const command = require(filePath);
-// 	// Set a new item in the Collection with the key as the command name and the value as the exported module
-// 	if ('data' in command && 'execute' in command) {
-// 		client.commands.set(command.data.name, command);
-// 	}
-// 	else {
-// 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-// 	}
-// }
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	}
+	else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
-// // Register Events
-// const eventsPath = path.join(__dirname, 'events');
-// const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+// Register Events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-// for (const file of eventFiles) {
-// 	const filePath = path.join(eventsPath, file);
-// 	const event = require(filePath);
-// 	if (event.once) {
-// 		client.once(event.name, (...args) => event.execute(...args));
-// 	}
-// 	else {
-// 		client.on(event.name, (...args) => event.execute(...args));
-// 	}
-// }
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
-// // Log in to Discord with your client's token
-// client.login(token);
-
-// // Webserver stuff
-// const express = require('express');
-// const server = express();
-// const port = process.env.PORT || 3001;
-
-// server.get('/', (req, res) => res.type('html').send('<html><body><h1>Page?</h1></body></html>'));
-// server.get('/healthy', (req, res) => res.status(200).type('html').send('<html><body><h1>Healthy</h1></body></html>'));
-// server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Log in to Discord with your client's token
+client.login(token);
 
 const express = require('express');
 const app = express();
@@ -63,59 +54,8 @@ const port = process.env.PORT || 3001;
 
 app.listen(port, '0.0.0.0');
 app.get('/', (req, res) => {
-	res.status(200).type('html').send(html);
+	res.status(200).type('html').send('<html><body><h1>Komfy Bot.</h1></body></html>');
 });
 app.get('/healthy', (req, res) => {
 	res.status(200).type('html').send('<html><body><h1>Healthy</h1></body></html>');
 });
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`;
