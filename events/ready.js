6299d8ee7e8a39ec97fc3ecbd01da815e714e5f6
Angie <angiejschmitt@gmail.com>
2022-11-24 22:13:11 -0500

    Stuff for render

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,22 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		client.user.setActivity('activity', { type: ActivityType.Listening });
+		client.user.setPresence(
+			{ activities:
+				[
+					{
+						name: 'lo-fi beats.',
+						type: ActivityType.Listening,
+					},
+				],
+			status: 'idle',
+			});
+
 	},
 };
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 
 // Require the necessary discord.js classes
 const { Client, Partials, GatewayIntentBits, Collection } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('/etc/secrets/config.json');
 
 // Create a new client instance
 const client = new Client({
