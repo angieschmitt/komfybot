9d3ea8a6916b4efc92d15face3dd8e3021ef481f
Angie <angiejschmitt@gmail.com>
2023-06-05 08:26:04 -0400

    quick adjust for testing

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,27 @@
+require('../globals');
+
 const axios = require('axios');
 const { Events, ActivityType, EmbedBuilder, roleMention } = require('discord.js');
+
+const { channels, notifications } = require(configFile); // eslint-disable-line
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		client.user.setActivity('activity', { type: ActivityType.Listening });
+		client.user.setPresence({
+			activities:
+				[
+					{
+						name: 'lo-fi beats.',
+						type: ActivityType.Listening,
+					},
+				],
+			status: 'idle',
+		});
 
 		// 5 minutes : 300000
 		// 5 seconds : 5000
@@ -14,7 +30,7 @@ module.exports = {
 
 			if (x.is_live && started_at !== x.started_at) {
 
-				const twitch = roleMention('1048776612042260540');
+				const twitch = roleMention(notifications.twitch);
 				const embed = new EmbedBuilder()
 					.setColor(0xC44578)
 					.setAuthor({ name: x.user_name, iconURL: x.user_thumbnail })
@@ -27,27 +43,40 @@ module.exports = {
 					)
 					.setImage(x.thumbnail_url);
 
-				client.channels.fetch('1045318121839407154')
+				client.channels.fetch(channels.is_live)
 					.then(channel => {
 						channel.send({ content: `Hey ${twitch}, ${x.user_name} has gone live at https://www.twitch.tv/${x.user_login}.`, embeds: [embed] });
 					});
 
 				started_at = x.started_at;
 
+				client.user.setActivity('activity', { type: ActivityType.Streaming });
+				client.user.setPresence({
+					activities:
+						[
+							{
+								name: ' '.x.user_name,
+								type: ActivityType.Streaming,
+							},
+						],
+					status: 'idle',
+				});
+
+			}
+			else {
+				client.user.setActivity('activity', { type: ActivityType.Listening });
+				client.user.setPresence({
+					activities:
+						[
+							{
+								name: 'lo-fi beats.',
+								type: ActivityType.Listening,
+							},
+						],
+					status: 'idle',
+				});
 			}
 
-		});
-
-		client.user.setActivity('activity', { type: ActivityType.Listening });
-		client.user.setPresence({
-			activities:
-				[
-					{
-						name: 'lo-fi beats.',
-						type: ActivityType.Listening,
-					},
-				],
-			status: 'idle',
 		});
 
 	},
