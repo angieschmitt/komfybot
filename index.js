08ca9f2ddff605d46ec0c2b6b13a0091b19da172
Angie <angiejschmitt@gmail.com>
2023-08-11 12:42:23 -0400

    break it all

--- index.js
+++ index.js
@@ -15,49 +15,51 @@ const client = new Client({
 });
 
 // Assign Commands
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+// client.commands = new Collection();
+// const commandsPath = path.join(__dirname, 'commands');
+// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	}
-	else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
+// for (const file of commandFiles) {
+// 	const filePath = path.join(commandsPath, file);
+// 	const command = require(filePath);
+// 	// Set a new item in the Collection with the key as the command name and the value as the exported module
+// 	if ('data' in command && 'execute' in command) {
+// 		client.commands.set(command.data.name, command);
+// 	}
+// 	else {
+// 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+// 	}
+// }
 
 // Register Events
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+// const eventsPath = path.join(__dirname, 'events');
+// const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
+// for (const file of eventFiles) {
+// 	const filePath = path.join(eventsPath, file);
+// 	const event = require(filePath);
+// 	if (event.once) {
+// 		client.once(event.name, (...args) => event.execute(...args));
+// 	}
+// 	else {
+// 		client.on(event.name, (...args) => event.execute(...args));
+// 	}
+// }
+
+console.log(token);
 
 // Log in to Discord with your client's token
 client.login(token);
 
 // Webserver
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001;
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3001;
 
-app.listen(port, '0.0.0.0');
-app.get('/', (req, res) => {
-	res.status(200).type('html').send('<html><body><h1>Komfy Bot.</h1></body></html>');
-});
-app.get('/healthy', (req, res) => {
-	res.status(200).type('html').send('<html><body><h1>Healthy</h1></body></html>');
-});
+// app.listen(port, '0.0.0.0');
+// app.get('/', (req, res) => {
+// 	res.status(200).type('html').send('<html><body><h1>Komfy Bot.</h1></body></html>');
+// });
+// app.get('/healthy', (req, res) => {
+// 	res.status(200).type('html').send('<html><body><h1>Healthy</h1></body></html>');
+// });
