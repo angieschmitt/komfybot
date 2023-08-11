62b95d90fd89614307e0ca3e6eb89e2907e5e448
Angie <angiejschmitt@gmail.com>
2023-08-11 12:30:10 -0400

    ???

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ for (const file of commandFiles) {
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-console.log( eventsPath );
+console.log( 'Test:' + eventsPath );
 
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
