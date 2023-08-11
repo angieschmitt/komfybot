79deac2eaddbf99f7f61729a0190eb150463e166
Angie <angiejschmitt@gmail.com>
2023-08-11 12:38:59 -0400

    strip stuff to test

--- events/ready.js
+++ events/ready.js
@@ -23,55 +23,55 @@ module.exports = {
 
 		// 5 minutes : 300000
 		// 5 seconds : 5000
-		setInterval(function() {
-			axios.get('https://www.kittenangie.com/bots/api/twitch_live.php')
-				.then(function(response) {
-					if (response.data !== '') {
+		// setInterval(function() {
+		// 	axios.get('https://www.kittenangie.com/bots/api/twitch_live.php')
+		// 		.then(function(response) {
+		// 			if (response.data !== '') {
 
-						const x = response.data;
-						const { channels, notifications } = require(configFile); // eslint-disable-line
+		// 				const x = response.data;
+		// 				const { channels, notifications } = require(configFile); // eslint-disable-line
 
-						const twitch = roleMention(notifications.twitch);
-						const recommends = roleMention(notifications.recommends);
-						const embed = new EmbedBuilder()
-							.setColor(0xC44578)
-							.setAuthor({ name: x.user_name, iconURL: x.user_thumbnail })
-							.setTitle((x.title != '' ? x.title : 'Title goes here'))
-							.setURL('https://www.twitch.tv/' + x.user_login)
-							.setThumbnail(x.user_thumbnail)
-							.setDescription(`Currently playing: ${x.game_name}!`)
-							.setImage(x.thumbnail_url);
+		// 				const twitch = roleMention(notifications.twitch);
+		// 				const recommends = roleMention(notifications.recommends);
+		// 				const embed = new EmbedBuilder()
+		// 					.setColor(0xC44578)
+		// 					.setAuthor({ name: x.user_name, iconURL: x.user_thumbnail })
+		// 					.setTitle((x.title != '' ? x.title : 'Title goes here'))
+		// 					.setURL('https://www.twitch.tv/' + x.user_login)
+		// 					.setThumbnail(x.user_thumbnail)
+		// 					.setDescription(`Currently playing: ${x.game_name}!`)
+		// 					.setImage(x.thumbnail_url);
 
-						if (x.user_name.toLowerCase() == 'komfykiwi') {
-							client.channels.fetch(channels.is_live)
-								.then(channel => {
-									channel.send({
-										content: `Hey ${twitch}, ${ escapeMarkdown(x.user_name) } has gone live at https://www.twitch.tv/${x.user_login}.`,
-										embeds: [embed],
-									}).then(() => {
-										axios.get('https://www.kittenangie.com/bots/api/twitch_live.php?pinged=' + x.user_id);
-									});
-								});
-						}
-						else {
-							client.channels.fetch(channels.recommends)
-								.then(channel => {
-									channel.send({
-										content: `Hey ${recommends}, ${ escapeMarkdown(x.user_name) } has gone live at https://www.twitch.tv/${x.user_login}.`,
-										embeds: [embed],
-									}).then(() => {
-										axios.get('https://www.kittenangie.com/bots/api/twitch_live.php?pinged=' + x.user_id);
-									});
-								});
-						}
-					}
-				})
-				.catch(err => console.log(err));
-			axios.get(global.baseUrl + 'insert/channel_points/')
-				.then(() => {
-					axios.get(global.baseUrl + 'interactive/lights/');
-				})
-				.catch(err => console.log(err));
-		}, 10000);
+		// 				if (x.user_name.toLowerCase() == 'komfykiwi') {
+		// 					client.channels.fetch(channels.is_live)
+		// 						.then(channel => {
+		// 							channel.send({
+		// 								content: `Hey ${twitch}, ${ escapeMarkdown(x.user_name) } has gone live at https://www.twitch.tv/${x.user_login}.`,
+		// 								embeds: [embed],
+		// 							}).then(() => {
+		// 								axios.get('https://www.kittenangie.com/bots/api/twitch_live.php?pinged=' + x.user_id);
+		// 							});
+		// 						});
+		// 				}
+		// 				else {
+		// 					client.channels.fetch(channels.recommends)
+		// 						.then(channel => {
+		// 							channel.send({
+		// 								content: `Hey ${recommends}, ${ escapeMarkdown(x.user_name) } has gone live at https://www.twitch.tv/${x.user_login}.`,
+		// 								embeds: [embed],
+		// 							}).then(() => {
+		// 								axios.get('https://www.kittenangie.com/bots/api/twitch_live.php?pinged=' + x.user_id);
+		// 							});
+		// 						});
+		// 				}
+		// 			}
+		// 		})
+		// 		.catch(err => console.log(err));
+		// 	axios.get(global.baseUrl + 'insert/channel_points/')
+		// 		.then(() => {
+		// 			axios.get(global.baseUrl + 'interactive/lights/');
+		// 		})
+		// 		.catch(err => console.log(err));
+		// }, 10000);
 	},
 };
--- index.js
+++ index.js
@@ -35,8 +35,6 @@ for (const file of commandFiles) {
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-console.log( 'Test:' + eventsPath );
-
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
