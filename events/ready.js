53247dbf875294b0ad0c6b6ffb4550b8ee6b0c2c
Angie <angiejschmitt@gmail.com>
2023-09-14 10:56:39 -0400

    move to async function, add timeouts?

--- events/ready.js
+++ events/ready.js
@@ -24,54 +24,62 @@ module.exports = {
 		// 5 minutes : 300000
 		// 5 seconds : 5000
 		setInterval(function() {
-			// Live check
-			const cacheBuster = new Date().getTime();
-			axios.get('https://www.kittenangie.com/bots/api/twitch_live.php?&timestamp=' + cacheBuster)
-				.then(function(response) {
-					if (response.data !== '') {
 
-						const x = response.data;
-						const { channels, notifications } = require(configFile); // eslint-disable-line
-
-						const twitch = roleMention(notifications.twitch);
-						const recommends = roleMention(notifications.recommends);
-						const embed = new EmbedBuilder()
-							.setColor(0xC44578)
-							.setAuthor({ name: x.user_name, iconURL: x.user_thumbnail })
-							.setTitle(escapeMarkdown(x.title != '' ? x.title : 'Title goes here'))
-							.setURL('https://www.twitch.tv/' + x.user_login)
-							.setThumbnail(x.user_thumbnail)
-							.setDescription(`Currently playing: ${x.game_name}!`)
-							.setImage(x.thumbnail_url);
-
-						axios.get('https://www.kittenangie.com/bots/api/twitch_live.php?pinged=' + x.user_id)
-							.then(function(response2) {
-								if (response2.data.status === 'success') {
-									const pingChannel = (x.user_name.toLowerCase() == 'komfykiwi' ? channels.is_live : channels.recommends);
-									const pingWho = (x.user_name.toLowerCase() == 'komfykiwi' ? twitch : recommends);
-									client.channels.fetch(pingChannel)
-										.then(channel => {
-											channel.send({
-												content: `Hey ${pingWho}, ${ escapeMarkdown(x.user_name) } has gone live at https://www.twitch.tv/${x.user_login}.`,
-												embeds: [embed],
-											});
-										});
-								}
-							})
-							.catch(err => console.log(err));
-					}
-				})
-				.catch(err => console.log(err));
-
-			// Check for channel points
-			axios.get(global.baseUrl + 'insert/channel_points/')
-				.then(() => {
-					axios.get(global.baseUrl + 'interactive/lights/');
-				})
-				.catch(err => console.log(err));
+			handleLiveCheck(client);
+			handleChannelPoints();
 
 			// Update chatbot token
-			axios.get(global.baseUrl + 'generate/token.php?key=komfybot_token');
+			axios.get(global.baseUrl + 'generate/token.php?key=komfybot_token', { timeout: 5000 });
 		}, 10000);
 	},
 };
+
+async function handleLiveCheck(client) {
+	// Live check
+	const cacheBuster = new Date().getTime();
+	axios.get('https://www.kittenangie.com/bots/api/twitch_live.php?&timestamp=' + cacheBuster, { timeout: 5000 })
+		.then(function(response) {
+			if (response.data !== '') {
+
+				const x = response.data;
+				const { channels, notifications } = require(configFile); // eslint-disable-line
+
+				const twitch = roleMention(notifications.twitch);
+				const recommends = roleMention(notifications.recommends);
+				const embed = new EmbedBuilder()
+					.setColor(0xC44578)
+					.setAuthor({ name: x.user_name, iconURL: x.user_thumbnail })
+					.setTitle(escapeMarkdown(x.title != '' ? x.title : 'Title goes here'))
+					.setURL('https://www.twitch.tv/' + x.user_login)
+					.setThumbnail(x.user_thumbnail)
+					.setDescription(`Currently playing: ${x.game_name}!`)
+					.setImage(x.thumbnail_url);
+
+				axios.get('https://www.kittenangie.com/bots/api/twitch_live.php?pinged=' + x.user_id)
+					.then(function(response2) {
+						if (response2.data.status === 'success') {
+							const pingChannel = (x.user_name.toLowerCase() == 'komfykiwi' ? channels.is_live : channels.recommends);
+							const pingWho = (x.user_name.toLowerCase() == 'komfykiwi' ? twitch : recommends);
+							client.channels.fetch(pingChannel)
+								.then(channel => {
+									channel.send({
+										content: `Hey ${pingWho}, ${ escapeMarkdown(x.user_name) } has gone live at https://www.twitch.tv/${x.user_login}.`,
+										embeds: [embed],
+									});
+								});
+						}
+					})
+					.catch(err => console.log(err));
+			}
+		})
+		.catch(err => console.log(err));
+}
+
+async function handleChannelPoints() {
+	// Check for channel points
+	axios.get(global.baseUrl + 'insert/channel_points/', { timeout: 5000 })
+		.then(() => {
+			axios.get(global.baseUrl + 'interactive/lights/');
+		})
+		.catch(err => console.log(err));
+}
