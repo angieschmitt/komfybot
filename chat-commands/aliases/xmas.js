c490ed1b9798ad82436303ec365c9261c7491bc4
Angie <angiejschmitt@gmail.com>
2023-12-04 10:04:57 -0500

    npm update

---
+++ chat-commands/aliases/xmas.js
@@ -0,0 +1,4 @@
+module.exports = {
+	name: 'xmas',
+	alias: 'christmas',
+};
---
+++ chat-commands/basic/christmas.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'december',
+	description: 'December details',
+	help: 'December promo',
+	disabled: true,
+	actions: {
+		default: {
+			say: 'Hey, if you cheer exactly 100 Bits in the Month of December, you can get YOUR NAME ' +
+            'on an ornament on the tree behind Kiwi! Limited qty, so get yours while you still can!',
+		},
+	},
+};
