b40d7bf43f15a8b23e404ff01dab4b4ae6d981b1
Angie <angiejschmitt@gmail.com>
2023-12-04 11:24:30 -0500

    adjust for new hosting

--- globals.js
+++ globals.js
@@ -2,9 +2,6 @@ global.title = 'KiwiBot';
 global.baseUrl = 'https://kittenangie.com/bots/api_new/';
 global.baseUrl2 = 'https://kittenangie.com/bots/1.0/';
 
-let configFile = 'D:/Personal Sites/#GIT/komfy-bot/config.json';
-if (process.env.live === 'true') {
-	configFile = '/etc/secrets/config.json';
-}
+let configFile = __dirname + '/config.json';
 
 global.configFile = configFile;
