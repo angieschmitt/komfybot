ce24b1300a6fcb2160db19c5dac4033054b22368
Angie <angiejschmitt@gmail.com>
2024-04-22 13:15:21 -0400

    quiz

---
+++ twitch/data/trivia.js
@@ -0,0 +1,308 @@
+const trivia = [
+	{
+		'q':'A Moon stone turns Jigglypuff into what?',
+		'a':'Wigglytuff',
+	},
+	{
+		'q':'Amber is an admin for which team?',
+		'a':'Aqua',
+	},
+	{
+		'q':'Blastoise is what generation Pokemon?',
+		'a':'1st',
+	},
+	{
+		'q':'Caterpie is what type of Pokemon?',
+		'a':'Bug',
+	},
+	{
+		'q':'Charizard is an evolution of what?',
+		'a':'Charmeleon',
+	},
+	{
+		'q':'Charmander is what type of Pokemon?',
+		'a':'Fire',
+	},
+	{
+		'q':'Chimchar evolves into what?',
+		'a':'Monferno',
+	},
+	{
+		'q':'Drowzee is what type of Pokemon?',
+		'a':'Psychic',
+	},
+	{
+		'q':'Fennekin is a pure what?',
+		'a':'Fire',
+	},
+	{
+		'q':'Hoppip evolves into what at level 18?',
+		'a':'Skiploom',
+	},
+	{
+		'q':'Hoppip is a dual Grass/? Pokemon?',
+		'a':'Flying',
+	},
+	{
+		'q':'How many contest conditions are there?',
+		'a':'5',
+	},
+	{
+		'q':'How many orbs are there in total?',
+		'a':'6',
+	},
+	{
+		'q':'Igglybuff evolves into what?',
+		'a':'Jigglypuff',
+	},
+	{
+		'q':'Ivysaur evolves into which Pokemon?',
+		'a':'Venusaur',
+	},
+	{
+		'q':'Lucario is what dual type of Pokemon?',
+		'a':'Fighting/Steel',
+	},
+	{
+		'q':'Mankey is what type of Pokemon?',
+		'a':'Fighting',
+	},
+	{
+		'q':'Name the most famous yellow Pokemon?',
+		'a':'Pikachu',
+	},
+	{
+		'q':'Paras evolves into what at level 24?',
+		'a':'Parasect',
+	},
+	{
+		'q':'Phanpy is what type of Pokemon?',
+		'a':'Ground',
+	},
+	{
+		'q':'Pidgey evolves into which Pokemon?',
+		'a':'Pidgeotto',
+	},
+	{
+		'q':'Plusle is what type of Pokemon?',
+		'a':'Electric',
+	},
+	{
+		'q':'Pokemon is short for what?',
+		'a':'Pocket Monsters',
+	},
+	{
+		'q':'Poochyeena is what type of Pokemon?',
+		'a':'Dark',
+	},
+	{
+		'q':'Professor Burnet has what color eyes?',
+		'a':'Yellow',
+	},
+	{
+		'q':'Professor Elm comes from which region?',
+		'a':'Johto',
+	},
+	{
+		'q':'Quilava is an evolution of what?',
+		'a':'Cyndaquil',
+	},
+	{
+		'q':'Slugma is what type of Pokemon?',
+		'a':'Fire',
+	},
+	{
+		'q':'Team Aqua is found in which region?',
+		'a':'Hoenn',
+	},
+	{
+		'q':'Team Plasma\'s home is which region?',
+		'a':'Unova',
+	},
+	{
+		'q':'Team Rocket are found in which region?',
+		'a':'Kanto',
+	},
+	{
+		'q':'The leader of Team Galactic is...?',
+		'a':'Cyrus',
+	},
+	{
+		'q':'Togepi is a Fairy from what generation?',
+		'a':'2nd',
+	},
+	{
+		'q':'Torterra evolves into what?',
+		'a':'Nobody',
+	},
+	{
+		'q':'Totodile evolves into what?',
+		'a':'Croconaw',
+	},
+	{
+		'q':'Turtwig is what type of Pokemon?',
+		'a':'Grass',
+	},
+	{
+		'q':'Vulpix is what type of Pokemon?',
+		'a':'Fire',
+	},
+	{
+		'q':'What color is James from Team Rocket\'s hair?',
+		'a':'Blue',
+	},
+	{
+		'q':'What color is the Rock/Ground Golem?',
+		'a':'Brown',
+	},
+	{
+		'q':'What does Oddish evolve into?',
+		'a':'Gloom',
+	},
+	{
+		'q':'What does Spearow evolve into?',
+		'a':'Fearow',
+	},
+	{
+		'q':'What is the English motto of Pokemon?',
+		'a':'Gotta Catch \'Em All!',
+	},
+	{
+		'q':'What is the newest type of Pokemon?',
+		'a':'Fairy',
+	},
+	{
+		'q':'What Pokedex number is Pikachu?',
+		'a':'25',
+	},
+	{
+		'q':'What species is Pikachu?',
+		'a':'Mouse',
+	},
+	{
+		'q':'What stone turns Gloom into Vileplume?',
+		'a':'Leaf',
+	},
+	{
+		'q':'What type of Pokemon is Happiny?',
+		'a':'Normal',
+	},
+	{
+		'q':'What type of Pokemon is Pachirisu?',
+		'a':'Electric',
+	},
+	{
+		'q':'What type of Pokemon is Pikachu?',
+		'a':'Electric',
+	},
+	{
+		'q':'What type of Pokemon is Tyrogue?',
+		'a':'Fighting',
+	},
+	{
+		'q':'What type of Pokemon is Venonat?',
+		'a':'Bug/Poison',
+	},
+	{
+		'q':'Which of the following is a Bug Pokemon?',
+		'a':'Illumise',
+	},
+	{
+		'q':'Which of these are Pokemon games?',
+		'a':'Gold & Silver',
+	},
+	{
+		'q':'Which Pokemon has a burning tail?',
+		'a':'Charmander',
+	},
+	{
+		'q':'Which region does Team Flare call home?',
+		'a':'Kalos',
+	},
+	{
+		'q':'Which team first appeared in Gen. LV?',
+		'a':'Galactic',
+	},
+	{
+		'q':'Which type of move is Acid Armor?',
+		'a':'Poison',
+	},
+	{
+		'q':'Which type of move is Ancient Power?',
+		'a':'Rock',
+	},
+	{
+		'q':'Which type of move is Arm Thrust?',
+		'a':'Fighting',
+	},
+	{
+		'q':'Which type of move is Body Slam?',
+		'a':'Normal',
+	},
+	{
+		'q':'Which type of move is Confuse Ray?',
+		'a':'Ghost',
+	},
+	{
+		'q':'Which type of Pokemon can beat Lorelei?',
+		'a':'Fire',
+	},
+	{
+		'q':'Whismur is what type of Pokemon?',
+		'a':'Normal',
+	},
+	{
+		'q':'Who are the trainers in Team Rocket?',
+		'a':'James & Jessie',
+	},
+	{
+		'q':'Who does Charmander evolve into?',
+		'a':'Charmeleon',
+	},
+	{
+		'q':'Who has a Psyduck that\'s a real nuisance?',
+		'a':'Misty',
+	},
+	{
+		'q':'Who is #1 in the Pokedex?',
+		'a':'Bulbasaur',
+	},
+	{
+		'q':'Who is the leader of Team Aqua?',
+		'a':'Archie',
+	},
+	{
+		'q':'Who is the leader of Team Rocket?',
+		'a':'Giovanni',
+	},
+	{
+		'q':'Who is the only Pokemon able to speak?',
+		'a':'Meowth',
+	},
+	{
+		'q':'Who was Ash\'s starter Pokemon?',
+		'a':'Pikachu',
+	},
+	{
+		'q':'Who worked with Gary at the Sayda Lab?',
+		'a':'Dora',
+	},
+	{
+		'q':'Xerosic is an admin for which team?',
+		'a':'Flare',
+	},
+	{
+		'q':'Zigzagoon evolves into what?',
+		'a':'Linoone',
+	},
+	{
+		'q':'Zinzolin is an admin for which team?',
+		'a':'Plasma',
+	},
+];
+
+module.exports = {
+	content: function() {
+		return trivia;
+	},
+};
