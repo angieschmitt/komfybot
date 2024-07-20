e7d03a919763fc3fef436d4d7b39227d6385b974
Angie <angiejschmitt@gmail.com>
2024-07-20 17:17:45 -0400

    move reset to dev cause less chaos

--- twitch/index-dev.js
+++ twitch/index-dev.js
@@ -25,11 +25,12 @@ if (Object.keys(extArgs).length !== 0) {
 	if (extArgs[0] === 'reset') {
 		// Handle Reset
 		const handleReset = new Promise((resolve) => {
-			axios.get(data.settings.baseUrl + 'insert/uptime')
+			const twitchData = { 'ident_type':'twitch_username', 'ident':'komfykiwi' };
+			axios.get(data.settings.newUrl + 'uptime/insert/json/' + encodeURIComponent(JSON.stringify(twitchData)))
 				.then(() => {
-					axios.get(data.settings.baseUrl + 'insert/guesses?reset')
+					axios.get(data.settings.newUrl + 'guess/reset/komfykiwi')
 						.then(() => {
-							axios.get(data.settings.baseUrl + 'insert/count?reset')
+							axios.get(data.settings.newUrl + 'count/reset/komfykiwi')
 								.then(() => {
 									axios.get(data.settings.newUrl + 'racers/reset')
 										.then(() => {
@@ -41,7 +42,7 @@ if (Object.keys(extArgs).length !== 0) {
 		});
 
 		handleReset.then(() => {
-			// data.functions.handleTimers(data, data.timers, client);
+			data.functions.handleTimers(data, data.timers, client);
 			data.debug.write('global', 'RESET');
 		});
 	}
