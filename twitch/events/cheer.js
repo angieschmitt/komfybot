e1e07d19ecdaf36d07084ae1524641f5fc24c1ef
Angie <schmitt@towermarketing.net>
2024-10-30 10:09:50 -0400

    Add Stream together catch :)

--- twitch/events/cheer.js
+++ twitch/events/cheer.js
@@ -3,9 +3,19 @@
 
 module.exports = {
 	eventHandler(channel, tags, message) {
-		console.log('caught cheer');
-		console.log(channel);
-		console.log(tags);
-		console.log(message);
+
+		const client = this;
+
+		if (tags['room-id'] === tags['source-room-id']) {
+
+			const bitText = (parseInt(tags['bits']) > 1 ? 'bits' : 'bit');
+
+			client.say(channel, `Thanks for the ${tags['bits']} ${bitText} ${tags['username']}!`);
+
+			console.log('caught cheer');
+			console.log(channel);
+			console.log(tags);
+			console.log(message);
+		}
 	},
 };
