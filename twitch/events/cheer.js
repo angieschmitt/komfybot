779a74c9c5eeaf1e3c128e9e0e8a2d3a2a5d1484
Angie <schmitt@towermarketing.net>
2024-10-30 15:25:00 -0400

    Of course they don't send it when no co-streaming

--- twitch/events/cheer.js
+++ twitch/events/cheer.js
@@ -6,16 +6,24 @@ module.exports = {
 
 		const client = this;
 
-		if (tags['room-id'] === tags['source-room-id']) {
-
-			const bitText = (parseInt(tags['bits']) > 1 ? 'bits' : 'bit');
-
-			client.say(channel, `Thanks for the ${tags['bits']} ${bitText} ${tags['username']}!`);
-
-			console.log('caught cheer');
-			console.log(channel);
-			console.log(tags);
-			console.log(message);
+		if ('source-room-id' in tags) {
+			if (tags['room-id'] === tags['source-room-id']) {
+				console.log('caught ' + channel + ' cheer');
+				const bitText = (parseInt(tags['bits']) > 1 ? 'bits' : 'bit');
+				client.say(channel, `Thanks for the ${tags['bits']} ${bitText} ${tags['username']}!`);
+			}
+			else {
+				console.log('caught co-streamer cheer');
+			}
 		}
+		else {
+			console.log('caught ' + channel + ' cheer');
+			const bitText = (parseInt(tags['bits']) > 1 ? 'bits' : 'bit');
+			client.say(channel, `Thanks for the ${tags['bits']} ${bitText} ${tags['username']}!`);
+		}
+
+		console.log(channel);
+		console.log(tags);
+		console.log(message);
 	},
 };
