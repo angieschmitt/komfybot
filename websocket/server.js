fdadfa02fe73d3429c126d0f0cff26c76ff0b334
Angie <schmitt@towermarketing.net>
2025-04-15 16:41:31 -0400

    Websocket adjustments

--- websocket/server.js
+++ websocket/server.js
@@ -1,13 +1,17 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+function init() {
+    const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
+    wss.on('connection', function connection(ws) {
+        ws.on('error', console.error);
 
-    ws.on('message', function message(data) {
-        console.log('Received: %s', data);
+        ws.on('message', function message(data) {
+            console.log('Received: %s', data);
+
+            ws.send('Received on server');
+        });
     });
+}
 
-    ws.send('Received on server');
-});
+init();
