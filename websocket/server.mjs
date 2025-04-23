29357a3c375c94840c2767b3daf40d8621fe1801
Angie <schmitt@towermarketing.net>
2025-04-23 12:05:29 -0400

    Hopefully?!

--- websocket/server.mjs
+++ websocket/server.mjs
@@ -5,14 +5,88 @@ import { WebSocketServer } from 'ws';
 function init() {
 
     const server = createServer({
-        cert: readFileSync('certs/server-key.pem'),
-        key: readFileSync('certs/server-cert.pem')
+        cert: readFileSync('websocket/certs/server-cert.pem'),
+        key: readFileSync('websocket/certs/server-key.pem')
     });
     const wss = new WebSocketServer({ server });
 
+    let users = [];
     wss.on('connection', function connection(ws, req) {
 
-        console.log(ws);
+        // get userID...
+        var userID = req.url.substr(1);
+
+        // add to users
+        if (!Object.values(users).includes(userID)) {
+            users.push(userID);
+        } else {
+            const key = getKeyByValue(users, userID);
+            users.splice(key, 1);
+            users.push(userID);
+        }
+
+        ws.on('error', console.error);
+
+        ws.on('message', function message(data) {
+
+            let output = {
+                'userList' : users
+            };
+
+            if ( isJson(data) ){
+
+                const parsed = JSON.parse(data);
+
+                console.log(parsed);
+
+                let action = parsed['action'].toString();
+                let source = parsed['source'].toString();
+                let target = 999;
+
+                switch (action) {
+                    case 'all':
+                        break;
+                    case 'close':
+                        // remove closed connection
+                        users.splice(getKeyByValue(users, source), 1);
+                        output['userList'] = users;
+                        target = getKeyByValue(users, 'panel');
+                        action = 'refresh';
+                        break;
+                    case 'refresh':
+                        target = getKeyByValue(users, 'panel');
+                        break;
+                    default:
+                        target = getKeyByValue(users, action);
+                        break;
+                }
+
+                output['response'] = action;
+                if ('data' in parsed){
+                    output['data'] = parsed['data'].toString()
+                }
+
+                output['timestamp'] = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+                // Loop over all the connections because we have to anyway...
+                let iter = 0;
+                wss.clients.forEach(function (client) {
+                    if (iter == target || target == 999){
+                        client.send( JSON.stringify(output) );
+                    }
+                    iter++;
+                });
+            }
+            else {
+
+                console.log(data);
+
+                wss.clients.forEach(function (client) {
+                    output['response'] = 'OOGA: ' + Math.random();
+                    client.send( JSON.stringify(output) );
+                });
+            }
+        });
 
         ws.on('error', console.error);
     
@@ -22,85 +96,6 @@ function init() {
     
         ws.send('something');
     });
-
-    // const wss = new WebSocketServer({ port: 8080 });
-
-    // let users = [];
-    // wss.on('connection', function connection(ws, req) {
-
-    //     var userID = req.url.substr(1);
-
-    //     if (!Object.values(users).includes(userID)) {
-    //         users.push(userID);
-    //     } else {
-    //         const key = getKeyByValue(users, userID);
-    //         users.splice(key, 1);
-    //         users.push(userID);
-    //     }
-
-    //     ws.on('error', console.error);
-
-    //     ws.on('message', function message(data) {
-
-    //         let output = {
-    //             'userList' : users
-    //         };
-
-    //         if ( isJson(data) ){
-
-    //             const parsed = JSON.parse(data);
-
-    //             console.log(parsed);
-
-    //             let action = parsed['action'].toString();
-    //             let source = parsed['source'].toString();
-    //             let target = 999;
-
-    //             switch (action) {
-    //                 case 'all':
-    //                     break;
-    //                 case 'close':
-    //                     // remove closed connection
-    //                     users.splice(getKeyByValue(users, source), 1);
-    //                     output['userList'] = users;
-    //                     target = getKeyByValue(users, 'panel');
-    //                     action = 'refresh';
-    //                     break;
-    //                 case 'refresh':
-    //                     target = getKeyByValue(users, 'panel');
-    //                     break;
-    //                 default:
-    //                     target = getKeyByValue(users, action);
-    //                     break;
-    //             }
-
-    //             output['response'] = action;
-    //             if ('data' in parsed){
-    //                 output['data'] = parsed['data'].toString()
-    //             }
-
-    //             output['timestamp'] = new Date().toISOString().slice(0, 19).replace('T', ' ');
-
-    //             // Loop over all the connections because we have to anyway...
-    //             let iter = 0;
-    //             wss.clients.forEach(function (client) {
-    //                 if (iter == target || target == 999){
-    //                     client.send( JSON.stringify(output) );
-    //                 }
-    //                 iter++;
-    //             });
-    //         }
-    //         else {
-
-    //             console.log(data);
-
-    //             wss.clients.forEach(function (client) {
-    //                 output['response'] = 'OOGA: ' + Math.random();
-    //                 client.send( JSON.stringify(output) );
-    //             });
-    //         }
-    //     });
-    // });
 
     server.listen(8080);
 }
