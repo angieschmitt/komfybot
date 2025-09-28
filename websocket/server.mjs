99a2c9dd28efb144dd7fd269755c7a6efc377769
Angie <schmitt@towermarketing.net>
2025-09-28 10:41:20 -0400

    still no clue

--- websocket/server.mjs
+++ websocket/server.mjs
@@ -15,8 +15,8 @@ const actions = loadActions();
 function init() {
 
     const server = createServer({
-        cert: readFileSync('./websocket/certs/server-cert.pem'),
-        key: readFileSync('./websocket/certs/server-key.pem')
+        cert: readFileSync('./certs/server-cert.pem'),
+        key: readFileSync('./certs/server-key.pem')
     });
 
     const wss = new WebSocketServer({ server });
