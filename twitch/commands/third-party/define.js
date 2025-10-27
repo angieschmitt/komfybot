ca82708659155b5f111aa44c8ed7804d60d15dac
angieschmitt <angiejschmitt@email.com>
2025-10-27 10:20:22 -0400

    Def change

--- twitch/commands/third-party/define.js
+++ twitch/commands/third-party/define.js
@@ -41,16 +41,23 @@ module.exports = {
 							.then(function(response) {
 								const resData = response.data;
 
-								output = `@${tags.username}, ${lookup} can be defined as a... `;
-								const meanings = resData[0].meanings;
-								Object.entries(meanings).forEach(([key]) => {
-									output += `${meanings[key].partOfSpeech} : ${meanings[key].definitions[0].definition} || `;
-								});
+								if (message in resData) {
+									if (resData.message == 'Sorry pal, we couldn\'t find definitions for the word you were looking for.') {
+										output = `Sorry @${tags.username}, we couldn't find definitions for the word you were looking for.`;
+									}
+								}
+								else {
+									output = `@${tags.username}, ${lookup} can be defined as a... `;
+									const meanings = resData[0].meanings;
+									Object.entries(meanings).forEach(([key]) => {
+										output += `${meanings[key].partOfSpeech} : ${meanings[key].definitions[0].definition} || `;
+									});
 
-								output = output.substring(0, output.length - 3).trim();
+									output = output.substring(0, output.length - 3).trim();
 
-								if (resData[0].phonetic) {
-									output += ` It is pronounced like: ${resData[0].phonetic}`;
+									if (resData[0].phonetic) {
+										output += ` It is pronounced like: ${resData[0].phonetic}`;
+									}
 								}
 							})
 							.catch(function(caught) {
