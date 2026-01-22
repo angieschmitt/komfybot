2603d0924b02551811d80cf1990a49176fc54fff
angieschmitt <angiejschmitt@email.com>
2026-01-22 09:53:02 -0500

    Add debug command output

--- twitch-v2/commands/kittenangie/streamData.js
+++ twitch-v2/commands/kittenangie/streamData.js
@@ -10,8 +10,10 @@ module.exports = {
 			execute(args, tags, message, channel, client) {
 				let content = '';
 
-				content += 'Live: ' + (client.isLive ? 'Yes' : 'No') + ' ||';
+				content += ' Live: ' + (client.isLive ? 'Yes' : 'No') + ' ||';
+				content += ' Chatters: ' + (client.data.chatters.length ? client.data.chatters : 'None') + ' ||';
 				content = content.substring(0, content.length - 2);
+				content = content.trim();
 
 				client.say(channel, `${content}`);
 			},
