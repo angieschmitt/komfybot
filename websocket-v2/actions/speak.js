3502bc3e98b5b34e727d3a9bad3a1f6295e7bbac
angieschmitt <angiejschmitt@email.com>
2026-02-24 10:26:24 -0500

    speak action

---
+++ websocket-v2/actions/speak.js
@@ -0,0 +1,44 @@
+module.exports = {
+	name: 'ping',
+	actions: {
+		default: {
+			execute(users, groups, source, data) {
+
+				const userKeys = Object.keys(users);
+				const userKey = getKeyByValue(userKeys, data['target']);
+
+				let target = [ userKey ];
+				if (target[0] === undefined) {
+					// Let's check for a group...
+					const groupKeys = Object.keys(groups);
+					const groupKey = getKeyByValue(groupKeys, data['target']);
+					if (groupKey !== undefined) {
+						// Found the group
+						target = [];
+						Object.entries(users).forEach(([item]) => {
+							if (item.indexOf(data['target'] + ':') > -1) {
+								const tempKey = getKeyByValue(userKeys, item);
+								target.push(tempKey);
+							}
+						});
+					}
+				}
+
+				data['target'] = target;
+
+				const output = {
+					'data': data,
+					'target': target,
+					'source': source,
+				};
+
+				return output;
+			},
+		},
+	},
+};
+
+function getKeyByValue(object, value) {
+	return Object.keys(object).find(key => object[key] === value);
+}
+
