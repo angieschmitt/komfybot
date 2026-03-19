3f87cb570aac1a4e98f8855a25ce142dd836812f
angieschmitt <angiejschmitt@email.com>
2026-03-19 09:51:48 -0400

    Catch auth fail, retry

---
+++ twitch-v3/events/chatClient/onAuthenticationFailure.js
@@ -0,0 +1,14 @@
+import functionsFunc from '../../functions/index.js';
+let functions = functionsFunc();
+
+// channel, user, text, msg
+
+export default async function(client) {
+
+    client.chatClient = new ChatClient({ 'authProvider': client.botAuthProvider, channels: [userData.username] });
+    client.chatClient.connect();
+
+    functions.eventLoader(client, chat);
+
+    return client;
+};
--- twitch-v3/functions/eventHandler.js
+++ twitch-v3/functions/eventHandler.js
@@ -7,7 +7,7 @@ const require = createRequire(import.meta.url);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export async function eventLoader(client){
+export async function eventLoader(client, type = false){
 
     // If the data bucket doesn't exist...
     if (!('eventHandlers' in client)) {
@@ -17,29 +17,33 @@ export async function eventLoader(client){
     }
 
     // Load in eventsub events...
-    const eventsubEventsPath = path.join(__dirname, '../events/eventSub');
-    const eventsubEventsFolder = fs.readdirSync(eventsubEventsPath);
-    for (const file of eventsubEventsFolder) {
-        const eventName = path.parse(file).name;
-        const filePath = path.join(eventsubEventsPath, file);
-        const eventData = require(filePath);
+    if (type == false || type == 'events') {
+        const eventsubEventsPath = path.join(__dirname, '../events/eventSub');
+        const eventsubEventsFolder = fs.readdirSync(eventsubEventsPath);
+        for (const file of eventsubEventsFolder) {
+            const eventName = path.parse(file).name;
+            const filePath = path.join(eventsubEventsPath, file);
+            const eventData = require(filePath);
 
-        client.eventHandlers.eventSub[eventName] = client.eventsubListener[eventName](client.twitchUUID, event => {
-            eventData.default(event, client);
-        });
+            client.eventHandlers.eventSub[eventName] = client.eventsubListener[eventName](client.twitchUUID, event => {
+                eventData.default(event, client);
+            });
+        }
     }
 
     // Load in chatclient events...
-    const chatEventsPath = path.join(__dirname, '../events/chatClient');
-    const chatEventsFolders = fs.readdirSync(chatEventsPath);
-    for (const file of chatEventsFolders) {
-        const eventName = path.parse(file).name;
-        const filePath = path.join(chatEventsPath, file);
-        const eventData = require(filePath);
+    if (type == false || type == 'chat') {
+        const chatEventsPath = path.join(__dirname, '../events/chatClient');
+        const chatEventsFolders = fs.readdirSync(chatEventsPath);
+        for (const file of chatEventsFolders) {
+            const eventName = path.parse(file).name;
+            const filePath = path.join(chatEventsPath, file);
+            const eventData = require(filePath);
 
-        client.eventHandlers.chatClient[eventName] = client.chatClient[eventName](async (...args) => {
-            eventData.default(...args, client);
-        });
+            client.eventHandlers.chatClient[eventName] = client.chatClient[eventName](async (...args) => {
+                eventData.default(...args, client);
+            });
+        }
     }
 
     return client;
