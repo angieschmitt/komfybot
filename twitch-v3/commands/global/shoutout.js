9b659791f0a80bd0df9904286931d91d28e06364
angieschmitt <angiejschmitt@email.com>
2026-03-25 15:44:11 -0400

    Missed a period

--- twitch-v3/commands/global/shoutout.js
+++ twitch-v3/commands/global/shoutout.js
@@ -99,6 +99,10 @@ export const actions = {
                             else {
                                 content += '.';
                             }
+
+                        }
+                        else {
+                            content += '.';
                         }
 
                     }
