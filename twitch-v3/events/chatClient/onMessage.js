fcedd6b9981a9cb7ca84573db57bb07b6ee0ce32
angieschmitt <angiejschmitt@email.com>
2026-04-18 19:05:53 -0400

    Fix for shared chat issue, limit to watching JUST the streamers chat

--- twitch-v3/events/chatClient/onMessage.js
+++ twitch-v3/events/chatClient/onMessage.js
@@ -5,8 +5,20 @@ let functions = functionsFunc();
 
 export default async function(channel, user, text, msg, client) {
 
-    client.lastMessage = text;
+    // Assume we're handling the message...
+    let handleMessage = true;
 
-    functions.messageHandler(channel, user, text, msg, client);
+    // If we have a source-room-id...
+    if (msg.tags.get('source-room-id')) {
+        // And it doesn't match our channels id....
+        if(msg.tags.get('source-room-id') !== client.twitchUUID){
+            handleMessage = false;
+        }
+    }
+
+    if (handleMessage){
+        client.lastMessage = text;
+        functions.messageHandler(channel, user, text, msg, client);
+    }
 
 };
